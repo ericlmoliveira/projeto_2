1f98f1db40e929f6dd20da7b2c05f84f25b2ce81
Eric Lima <ericlhp4@gmail.com>
2026-01-17 06:11:57 -0300

    feat

--- myApp.js
+++ myApp.js
@@ -4,14 +4,14 @@ let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 
-app.get("/", function (req, res)) {
+app.get("/", function (req, res) {
     
     const caminho_absoluto = __dirname + "/views/index.html";
     res.sendFile(caminho_absoluto);
     res.json({message: "Hello World"});
 
 
-};  
+});  
 
 
 
