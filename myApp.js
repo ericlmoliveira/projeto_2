9d7cce5cca2109b80bc06e676f3bd10a87150f3b
Eric Lima <ericlhp4@gmail.com>
2026-01-17 05:57:26 -0300

    feat add res.json

--- myApp.js
+++ myApp.js
@@ -3,6 +3,11 @@ let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("json", function (req, res) {
+    
+    res.json({"message": "Hello json"});
+});
+
 app.get("/", function (req, res) {
     
     const caminho_absoluto = __dirname + "/views/index.html";
