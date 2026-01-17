1200299fab4d87e9af829f631a4064e2a4272419
Eric Lima <ericlhp4@gmail.com>
2026-01-17 05:46:47 -0300

    feat add app.use('/public')

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
     
